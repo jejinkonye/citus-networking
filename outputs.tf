4ded223e3bc27d876b6f1f3bd9f74731362160d3
Jude Ejinkonye <jejinkonye@gmail.com>
2024-02-09 14:55:52 -0700

    comment out variables and output config

--- outputs.tf
+++ outputs.tf
@@ -2,12 +2,12 @@
 # OUTPUT
 ##################################################################################
 
-output "vpc_id" {
-  value       = module.main.vpc_id
-  description = "VPC ID"
-}
+# output "vpc_id" {
+#   value       = module.main.vpc_id
+#   description = "VPC ID"
+# }
 
-output "public_subnet" {
-  value       = module.main.public_subnets
-  description = "List of public subnets"
-}
+# output "public_subnet" {
+#   value       = module.main.public_subnets
+#   description = "List of public subnets"
+# }
